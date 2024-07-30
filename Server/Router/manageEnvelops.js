561986c364a8128df0120a46f9831c4c7ce6cc2d
zackd44 <zackdavison1@outlook.com>
2024-07-30 17:44:20 +0100

    Changed prettier printwidth to 100

--- Server/Router/manageEnvelops.js
+++ Server/Router/manageEnvelops.js
@@ -9,39 +9,39 @@ const manageEnvelopeRouter = express.Router();
 
 //Route for creating envelope
 manageEnvelopeRouter.post('/create-envelope/:category', (req, res, next) => {
-    const newCategory = req.params.category;
-    // check if category already exists
-    if (envelopes[newCategory]) {
-        console.log('Category already exists');
-        res.status(400).json({ Reason: 'Envelope already exists' });
-    } else {
-        envelopes[newCategory] = {
-            budget: 0,
-        };
+  const newCategory = req.params.category;
+  // check if category already exists
+  if (envelopes[newCategory]) {
+    console.log('Category already exists');
+    res.status(400).json({ Reason: 'Envelope already exists' });
+  } else {
+    envelopes[newCategory] = {
+      budget: 0,
+    };
 
-        res.status(201).send(envelops[newCategory]);
-    }
+    res.status(201).send(envelops[newCategory]);
+  }
 });
 
 // Route for deleting envelope
 manageEnvelopeRouter.delete('/delete-envelope/:category', (req, res, next) => {
-    // category to delete
-    const categoryToDelete = req.params.category;
+  // category to delete
+  const categoryToDelete = req.params.category;
 
-    //if envelope in object delete it
-    if (envelopes[categoryToDelete]) {
-        delete envelopes[categoryToDelete];
-        res.status(200).json({
-            Response: `${categoryToDelete} Envelope Deleted`,
-        });
-    } else {
-        // if not send user error code
-        res.status(400).json({
-            Reason: 'Envelope provided was not an existing envelope',
-        });
-    }
+  //if envelope in object delete it
+  if (envelopes[categoryToDelete]) {
+    delete envelopes[categoryToDelete];
+    res.status(200).json({
+      Response: `${categoryToDelete} Envelope Deleted`,
+    });
+  } else {
+    // if not send user error code
+    res.status(400).json({
+      Reason: 'Envelope provided was not an existing envelope',
+    });
+  }
 });
 
 module.exports = {
-    manageEnvelopeRouter,
+  manageEnvelopeRouter,
 };
--- Server/Router/manipulateEnvelopeValues.js
+++ Server/Router/manipulateEnvelopeValues.js
@@ -7,27 +7,41 @@ const { envelopes } = require('../envelopes');
 const manipulateEnvelopeValueRouter = express.Router();
 
 // Add Money to envelope route
-manipulateEnvelopeValueRouter.post(
-  '/add-money/:category/:value',
-  (req, res, next) => {
-    const categoryToAddValueInObj = envelopes[req.params.category];
-    const valueToAddToEnvelope = Number(req.params.value);
+manipulateEnvelopeValueRouter.post('/add-money/:category/:value', (req, res, next) => {
+  const categoryToAddValueInObj = envelopes[req.params.category];
+  const valueToDeduceFromEnvelope = Number(req.params.value);
 
-    const categoryValue = req.params.category;
+  const categoryValue = req.params.category;
 
-    // if value is a number and category exists add value and send envelopes
-    if (valueToAddToEnvelope && categoryToAddValueInObj) {
-      envelopes[categoryValue]['budget'] += valueToAddToEnvelope;
-      res.json(envelopes[categoryValue]);
-    } else {
-      // Respond with error and possible reason and current existing categories
-      res.status(400).json({
-        Reason:
-          'Your value may not be a number or the category might not exist',
-        'All Existing Categories': Object.keys(envelopes),
-      });
-    }
-  },
-);
+  // if value is a number and category exists add value and send envelopes
+  if (valueToDeduceFromEnvelope && categoryToAddValueInObj) {
+    envelopes[categoryValue]['budget'] += valueToDeduceFromEnvelope;
+    res.json(envelopes[categoryValue]);
+  } else {
+    // Respond with error and possible reason and current existing categories
+    res.status(400).json({
+      Reason: 'Your value may not be a number or the category might not exist',
+      'All Existing Categories': Object.keys(envelopes),
+    });
+  }
+});
+
+manipulateEnvelopeValueRouter.post('/deduce-money/:category/:value', (req, res, next) => {
+  const categoryToAddValueInObj = envelopes[req.params.category];
+  const valueToDeduceFromEnvelope = Number(req.params.value);
+
+  const categoryValue = req.params.category;
+
+  // if value is a number and category exists deduce value and send envelopes
+  if (valueToDeduceFromEnvelope && categoryToAddValueInObj) {
+    const newBudgetForCategory = envelopes[categoryValue][budget] - valueToDeduceFromEnvelope;
+  } else {
+    // Respond with error and possible reason and current existing categories
+    res.status(400).json({
+      Reason: 'Your value may not be a number or the category might not exist',
+      'All Existing Categories': Object.keys(envelopes),
+    });
+  }
+});
 
 module.exports = manipulateEnvelopeValueRouter;
