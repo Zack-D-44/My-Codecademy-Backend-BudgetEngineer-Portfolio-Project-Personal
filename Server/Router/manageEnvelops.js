2b5b1229417a59da09cb330468014a4954f537fa
zackd44 <zackdavison1@outlook.com>
2024-07-30 17:27:58 +0100

    Installed prettier and create config file

--- Server/Router/manageEnvelops.js
+++ Server/Router/manageEnvelops.js
@@ -3,21 +3,21 @@
 const express = require('express');
 
 // import envelope js file
-const {envelopes} = require('../envelopes')
+const { envelopes } = require('../envelopes');
 
 const manageEnvelopeRouter = express.Router();
 
-//Route for creating envelope 
+//Route for creating envelope
 manageEnvelopeRouter.post('/create-envelope/:category', (req, res, next) => {
     const newCategory = req.params.category;
     // check if category already exists
-    if(envelopes[newCategory]){
-        console.log('Category already exists')
-        res.status(400).json({Reason: 'Envelope already exists'});
-    }else{
+    if (envelopes[newCategory]) {
+        console.log('Category already exists');
+        res.status(400).json({ Reason: 'Envelope already exists' });
+    } else {
         envelopes[newCategory] = {
-            budget: 0
-        }
+            budget: 0,
+        };
 
         res.status(201).send(envelops[newCategory]);
     }
@@ -27,18 +27,21 @@ manageEnvelopeRouter.post('/create-envelope/:category', (req, res, next) => {
 manageEnvelopeRouter.delete('/delete-envelope/:category', (req, res, next) => {
     // category to delete
     const categoryToDelete = req.params.category;
-    
-    //if envelope in object delete it 
-    if(envelopes[categoryToDelete]){
+
+    //if envelope in object delete it
+    if (envelopes[categoryToDelete]) {
         delete envelopes[categoryToDelete];
-        res.status(200).json({Response: `${categoryToDelete} Envelope Deleted`});
-    }else{
+        res.status(200).json({
+            Response: `${categoryToDelete} Envelope Deleted`,
+        });
+    } else {
         // if not send user error code
-        res.status(400).json({Reason: "Envelope provided was not an existing envelope"});
+        res.status(400).json({
+            Reason: 'Envelope provided was not an existing envelope',
+        });
     }
 });
 
-
-module.exports = { 
-    manageEnvelopeRouter
+module.exports = {
+    manageEnvelopeRouter,
 };
--- Server/envelopes.js
+++ Server/envelopes.js
@@ -1,17 +1,15 @@
 // This is the js file that will contain different envelops
 
 const envelopes = {
-    Rent: {
-        budget: 0
-    },
+  Rent: {
+    budget: 0,
+  },
 
-    Food: {
-        budget: 32        
-    }
-
-};
-
+  Food: {
+    budget: 32,
+  },
+}
 
 module.exports = {
-    envelopes
+  envelopes,
 }
