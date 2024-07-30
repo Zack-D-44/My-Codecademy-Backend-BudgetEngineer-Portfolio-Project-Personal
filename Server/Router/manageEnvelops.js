b4b0fd341327ee15b8fa7552dfe3e994b30abe82
zackd44 <zackdavison1@outlook.com>
2024-07-30 02:02:15 +0100

    Created and mounted manage envelope router

---
+++ Server/Router/manageEnvelops.js
@@ -0,0 +1,12 @@
+// Create and Delete Envelopes
+// Require express
+const express = require('express');
+
+const manageEnvelopeRouter = express.Router();
+
+manageEnvelopeRouter.post()
+
+
+module.exports = { 
+    manageEnvelopeRouter
+};
--- Server/server.js
+++ Server/server.js
@@ -1,7 +1,12 @@
 // Require express
 const express = require('express');
+const { manageEnvelopeRouter } = require('./Router/manageEnvelops');
 // Create express app instance
 const app = express();
+
+// Use manage Envelops router
+app.use('/manage-envelops', manageEnvelopeRouter);
+
 
 // Start server
 app.listen(4000, () => {
