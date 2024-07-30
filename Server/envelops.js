f94e809631dbe6ad16545be924ac7598a19a1b3c
zackd44 <zackdavison1@outlook.com>
2024-07-30 01:57:47 +0100

    Created envelops in js file

---
+++ Server/envelops.js
@@ -0,0 +1,17 @@
+// This is the js file that will contain different envelops
+
+const envelops = {
+    Rent: {
+        budget: 0
+    },
+
+    Food: {
+        budget: 0        
+    }
+
+};
+
+
+module.exports = {
+    envelops
+}
