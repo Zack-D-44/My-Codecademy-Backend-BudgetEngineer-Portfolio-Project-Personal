0386e29f0b216b14b8d32d9bece30f2a17fd7afa
zackd44 <zackdavison1@outlook.com>
2024-07-30 16:46:33 +0100

    Made view all envelopes route

--- Server/Router/viewEnvelopes.js
+++ Server/Router/viewEnvelopes.js
@@ -1,12 +1,14 @@
 // Require express
 const express = require('express');
+const { envelopes } = require('../envelopes');
 
 // Create Router
 const viewEnvelopesRouter = express.Router();
 
 // Route for viewing all envelopes
 viewEnvelopesRouter.get('/', (req, res, next) => {
-
+    // Send as json envelopes object 
+    res.status(200).json(envelopes);
 });
 
 // Route for viewing specific envelope
